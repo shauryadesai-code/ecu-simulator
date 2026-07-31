371d77c2e8ff724a4ebf7b43a90ee8527ba938d2
Shaurya Desai <shaurya.ideas@gmail.com>
2026-07-31 22:53:31 +0100

    Add update() and get_state() methods for vehicle physics and state reporting

--- vehicle.py
+++ vehicle.py
@@ -12,4 +12,23 @@ class Vehicle:
         self.gravity = 9.81  # in m/s^2
         self.velocity = 0.0  # in m/s
         self.position = 0.0  # in m
-    
+    def update(self, throttle, brake, road_grade, dt):
+        #throttle: 0 to 1, brake: 0 to 1, road_grade: in radians, dt: time step in seconds
+        drag_force = 0.5 * self.air_density * self.frontal_area * self.drag_coefficient * self.velocity ** 2
+        rolling_resistance_force = self.rolling_resistance_coefficient * self.mass * self.gravity * np.cos(road_grade)
+        grade_force = self.mass * self.gravity * np.sin(road_grade)
+        drive_force = throttle * self.max_drive_force
+        brake_force = brake * self.max_brake_force
+        net_force = drive_force - drag_force - rolling_resistance_force - grade_force - brake_force
+        acceleration = net_force / self.mass
+        self.velocity += acceleration * dt
+        if self.velocity < 0:
+            self.velocity = 0
+        self.position+= self.velocity * dt
+    def get_state(self):
+        return {
+            'speed_m/s': self.velocity,
+            'speed_mph': self.velocity * 2.237,
+            'position_m': self.position,
+            
+        }
